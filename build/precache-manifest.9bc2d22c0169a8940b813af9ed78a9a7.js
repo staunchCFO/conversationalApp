7d0bbaf54f5939ab78fc2c96e143bc51b40445de
Emeka Okezie <okezieemeka949@gmail.com>
2020-09-30 12:52:39 -0700

    My first conversational app

---
+++ build/precache-manifest.9bc2d22c0169a8940b813af9ed78a9a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1400fcf88a686d96e2113cd0b64697ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "748112eb2d4cae1fb657",
+    "url": "/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "ceed464c50c991bd84ec",
+    "url": "/static/js/2.311e4518.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.311e4518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748112eb2d4cae1fb657",
+    "url": "/static/js/main.56b82567.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
